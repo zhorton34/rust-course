ddc457503efb8606c431c33cd1d8f196780d6b86
Zak Horton <zhorton999@gmail.com>
2023-03-13 06:04:26 -0700

    Updated lessons

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn reverse(input: &str) -> String {
     let mut reversed = String::new();
 
-    for c in input.chars() {
-        reversed.insert(0, c);
+    for character in input.chars() {
+        reversed.insert(0, character);
     }
 
     reversed
