c3c37bd7a53bba0c5bd077ac7817c859fa647fc7
Zak Horton <zhorton999@gmail.com>
2023-03-13 06:36:52 -0700

    clock struct exercise completed

---
+++ clock/tests/clock.rs
@@ -0,0 +1,25 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let total_minutes = (hours * 60 + minutes) % (24 * 60);
+        let (hours, minutes) = (total_minutes / 60, total_minutes % 60);
+        Self { hours, minutes }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Self::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
